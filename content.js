41b681ef95b675a6a9d011af0d62858f764b3961
Matthias Hannig <matthias@hannig.cc>
2018-06-26 18:14:56 +0200

    some simple style modifications

---
+++ content.js
@@ -0,0 +1,12 @@
+
+Alice.updateContent({
+  header: {
+      title: "My Awesome Looking Glass",
+      tagline: "Alice rocks!"
+  },
+  welcome: {
+      title: "My Awesome Looking Glass",
+      tagline: "powered by Alice, your friendly BIRD Looking Glass"
+  }
+});
+
