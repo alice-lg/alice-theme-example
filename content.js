b0c74bb2b0b33e028058f6bc79470e9272191092
Jonathan Lassoff <jof@thejof.com>
2024-02-20 00:11:44 -0800

    Add lookup examples

--- content.js
+++ content.js
@@ -7,6 +7,14 @@ Alice.updateContent({
   welcome: {
       title: "My Awesome Looking Glass",
       tagline: "powered by Alice, your friendly BIRD||GoGBP||OpenBGPd Looking Glass"
+  },
+  lookup: {
+    examples: [
+      ["asn", "AS64512"],
+      ["community", 64512, 1000, 1],
+      ["community", 65520, 0],
+      ["q", "Example"],
+    ]
   }
 });
 
